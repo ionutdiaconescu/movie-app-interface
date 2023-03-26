52d5a3c49091b32cd2681dc8067fb20e6dbb2db7
ionutdiaconescu <ionutdiaconescu95@yahoo.com>
2023-03-26 17:22:56 +0300

    corect bugs

---
+++ src/MovieCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="movie">
+      <div>
+        <p>{movie.Year}</p>
+      </div>
+      <div>
+        <img
+          src={
+            movie.Poster !== "N/A"
+              ? movie.Poster
+              : "https://www.keekokids.co.uk/wp-content/uploads/placeholder-400x400.png"
+          }
+          alt={movie.Title}
+        />
+      </div>
+      <div>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
